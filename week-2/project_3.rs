d360b2196fd4865026a9e5bb07df4a21642e95a6
chiboy-star <chinemeremmadueke1@gmail.com>
2024-11-01 13:02:58 +0100

    (my third commit)

--- week-2/project_3.rs
+++ week-2/project_3.rs
@@ -2,7 +2,7 @@ fn main(){
     let p:f64 = 510000.00;
     let r:f64 = 5.0;
     // compound interest for depreciation
-    let a1:f64 = p*(1.0-(r/100.00));
+    let a1:f64 = p*(1.0-(r/100.00));//.powf(3.0);
     println!("The depreciation for the first year is {}",a1);
     let a2:f64 = a1*(1.0-(r/100.00));
     println!("The depreciation for the first year is {}",a2);
---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let fees = 25_000;
+    println!("fees is {} ",fees);
+
+    let fees = fees+10_000;
+    println!("fees changed is {}",fees);
+}
